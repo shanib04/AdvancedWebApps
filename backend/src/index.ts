78378652ba7075ad8bed670c00550ef9b0fbbb82
Itay Ram <itayram03@gmail.com>
2026-02-20 23:20:14 +0200

    feat: init frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import connectDB from "./config/db";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
@@ -12,6 +13,12 @@ const app = express();
 connectDB();
 
 // Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
