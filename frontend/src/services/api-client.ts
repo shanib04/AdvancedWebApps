53aef0b66ad15c87594a783c3b7ec75e95ef65f1
Itay Ram <itayram03@gmail.com>
2026-02-28 10:11:26 +0200

    feat: add icons, animation and api calles

--- frontend/src/services/api-client.ts
+++ frontend/src/services/api-client.ts
@@ -4,4 +4,14 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000",
 });
 
+apiClient.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (accessToken && accessToken !== "undefined" && accessToken !== "null") {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
 export default apiClient;
