ce75435d403fd61f3e142e371b0c911cf3a72917
Itay Ram <itayram03@gmail.com>
2026-03-04 15:37:21 +0200

    feat: add log back

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,8 @@ import app from "./index";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
